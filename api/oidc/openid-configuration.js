938181074ae7661b4ef9a3ee01df133dd3c45b07
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2025-07-06 00:58:53 +0100

    Update openid-configuration.js

--- api/oidc/openid-configuration.js
+++ api/oidc/openid-configuration.js
@@ -2,10 +2,10 @@ export default async function handler(req, res) {
   const baseUrl = process.env.BASE_URL || "http://localhost:3000/api/oidc";
   res.json({
     issuer: baseUrl,
-    authorization_endpoint: `${baseUrl}/authorize`,
-    token_endpoint: `${baseUrl}/token`,
-    userinfo_endpoint: `${baseUrl}/userinfo`,
-    jwks_uri: `${baseUrl}/jwks`,
+    authorization_endpoint: `${baseUrl}/api/oidc/authorize`,
+    token_endpoint: `${baseUrl}/api/oidc/token`,
+    userinfo_endpoint: `${baseUrl}/api/oidc/userinfo`,
+    jwks_uri: `${baseUrl}/api/oidc/jwks`,
     response_types_supported: ["code"],
     subject_types_supported: ["public"],
     id_token_signing_alg_values_supported: ["RS256"],
