39cb5fc0d166be4ea83d12c771c3047569773b76
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2025-07-06 02:01:21 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -98,27 +98,30 @@ export default function Home() {
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-  <a
-    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <Image
-      aria-hidden
-      src="/file.svg"
-      alt="File icon"
-      width={16}
-      height={16}
-    />
-    Learn
-  </a>
-  <a
-    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-    href="https://vercel.com/templates?framework=create-next-app"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Vercel Templates
-  </a>
-</footer>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vercel Templates
+        </a>
+      </footer>
+    </div>
+  );
+}
