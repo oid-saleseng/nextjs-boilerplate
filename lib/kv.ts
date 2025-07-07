519d9b93e462652a459c687644eb794185389481
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2025-07-07 12:08:16 +0100

    Update kv.ts

--- lib/kv.ts
+++ lib/kv.ts
@@ -1,6 +1,6 @@
-import { createClient } from "@vercel/kv";
+import { Redis } from '@upstash/redis';
 
-export const kv = createClient({
+export const kv = new Redis({
   url: process.env.KV_REST_API_URL!,
   token: process.env.KV_REST_API_TOKEN!,
 });
