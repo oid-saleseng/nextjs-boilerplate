593bec1ad43d1ae923bc8f9f14cd6a34a6277b23
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2025-07-14 13:57:09 +0100

    Create page.tsx

---
+++ app/sso/page.tsx
@@ -0,0 +1,10 @@
+import { Suspense } from "react";
+import SSOLogin from "./SSOLogin";
+
+export default function SSOPage() {
+  return (
+    <Suspense fallback={<div>Loading SSO Login form...</div>}>
+      <SSOPage />
+    </Suspense>
+  );
+}
