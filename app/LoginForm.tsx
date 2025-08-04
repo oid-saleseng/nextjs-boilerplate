28340a617cbf465eca77e63b89977311863ee6cf
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2025-08-04 23:22:50 +0100

    Update LoginForm.tsx

--- app/LoginForm.tsx
+++ app/LoginForm.tsx
@@ -147,7 +147,7 @@ export default function LoginForm() {
   className="w-full bg-white text-black border border-gray-300 rounded-md px-4 py-2 hover:bg-gray-100 transition-colors"
   onClick={() => {
     if (!email.trim()) {
-      alert("Please enter your email address to continue");
+      alert("Please enter your email address or Phone Number to continue");
     } else {
       alert("Trying passkey login");
       // You can initiate passkey login here
